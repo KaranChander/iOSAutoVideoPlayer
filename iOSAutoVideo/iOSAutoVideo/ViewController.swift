87dcc885239bfd0e31d15c08313de7e06b776be0
Karan Lnu <krnchander47@gmail.com>
2023-02-10 18:53:25 -0500

    files added

---
+++ iOSAutoVideo/iOSAutoVideo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSAutoVideo
+//
+//  Created by Karan . on 2/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
